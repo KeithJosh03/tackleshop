2020d0f2959e8191195bed5ebc2c82415601ce40
KeithJosh03 <kitjuswa0304@gmail.com>
2025-07-28 22:04:21 +0800

    testing axios http resquest to laravelbackend

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,14 +1,25 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import Image from 'next/image'
 import Link from 'next/link';         
-
 import { imagesAsset } from '@/types/image'
 
 import { worksans } from '@/types/fonts';
 
-
 export default function Header() {
+  const [brands, setBrands] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/brands')
+    .then(res => setBrands(res.data.brands))
+    .catch(err => console.log(err));
+  },[])
+
+  console.log(brands);
+
   return (
   <header className='top-0 fixed w-full z-50'>
     <nav className={`${worksans.className} text-primaryColor flex flex-col items-center bg-mainBackgroundColor`}>
